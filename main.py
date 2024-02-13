594b8a10c23a24d0fdb83bbf25c78e0b819a44f3
PangPangGod <rhcp1134@gmail.com>
2024-02-14 06:46:00 +0900

    .bat file path edit, test

--- main.py
+++ main.py
@@ -1,2 +1,11 @@
 with open("test.txt", "w+", encoding="utf-8") as file:
     file.write("Hello World!")
+
+from datetime import datetime
+now = datetime.now()
+current_time = now.strftime("%Y-%m-%d %H:%M:%S")
+
+print("="*60)
+print(f"Current time: {current_time}")
+print("test complete.")
+print("="*60)
--- run.bat
+++ run.bat
@@ -1,4 +1,4 @@
 @echo off
 CALL conda activate gemini
-python D:\PythonWorkSpace\git\side\main.py
+python D:\PythonWorkSpace\git\side\main.py >> D:\PythonWorkSpace\git\side\log.txt
 CALL conda deactivate
