1baa1be9c881dfe0a203117e6ce9afb23d56bb7d
PangPangGod <rhcp1134@gmail.com>
2025-03-10 21:39:02 +0900

    update deprecated packages

--- chain.py
+++ chain.py
@@ -1,5 +1,5 @@
 from typing import List
-from langchain_core.pydantic_v1 import BaseModel, Field
+from pydantic import BaseModel, Field
 
 from langchain.output_parsers import PydanticOutputParser
 from langchain_openai import ChatOpenAI
